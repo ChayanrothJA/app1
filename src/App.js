bc2e4cbadf4b1ae62a4ad4f341392db7b1737c48
ChayanrothJA <110073989+ChayanrothJA@users.noreply.github.com>
2023-08-11 14:21:16 +0700

    Create Function Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Header, Content, Footer} from './func-components'
 import './style.css'
 
 function App() {
   
   return (
-    <>
-    <h2>DeveloperThai.com</h2>
-    <div className='title'>React & React Native</div>
-    </>
+   <>
+   <Header/>
+   <Content/>
+   <Footer/>
+   </>
   )
 }
 
