8a1cc5fe2b386d01b7b4c194e82b95514119cb49
ChayanrothJA <110073989+ChayanrothJA@users.noreply.github.com>
2023-08-11 14:58:26 +0700

    Update Class Compolnent
    Create Calendar in class-component.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Header, Content, Footer} from './func-components'
-import './style.css'
+import Calendar from './class-components';
 
 function App() {
   
   return (
    <>
    <Header/>
+   <p><center><Calendar/></center></p>
    <Content/>
    <Footer/>
    </>
