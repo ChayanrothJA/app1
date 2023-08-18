9811d94e603fdabaaedf42968fda92df7dd43d47
Chayanroth_KMUTNB <110073989+ChayanrothJA@users.noreply.github.com>
2023-08-18 13:48:55 +0700

    add banner

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 import React from 'react'
+import Banner from './banner';
 import { Header, Content, Footer} from './func-components'
 import Calendar from './class-components';
 import MsgBox from './class-props';
 import MsgBox2 from './func-props';
 
-function App() {
+// function App() {
   
-  return (
-   <>
-   <Header/>
-   <p><center><Calendar/></center></p>
-   <Content/>
-   <Footer/>
-   <MsgBox
-      text='React & React Native'
-      color='blue'
-      bgColor='#ccc'
-      fontSize='18pt'
-      border='solid 1px black'
-   />
-   <MsgBox2
-      text='การใช้ props ใน Function Component'
-      color='red'
-      bgColor='#ccc'
-      fontSize='18pt'
-      border='solid 1px black'
-   />
+//   return (
+//    <>
+//    <Header/>
+//    <p><center><Calendar/></center></p>
+//    <Content/>
+//    <Footer/>
+//    <MsgBox
+//       text='React & React Native'
+//       color='blue'
+//       bgColor='#ccc'
+//       fontSize='18pt'
+//       border='solid 1px black'
+//    />
+//    <MsgBox2
+//       text='การใช้ props ใน Function Component'
+//       color='red'
+//       bgColor='#ccc'
+//       fontSize='18pt'
+//       border='solid 1px black'
+//    />
   
-   </>
+//    </>
 
-  )
+//   )
+// }
+
+export default function App(){
+   return <Banner/>
 }
-
-export default App;
