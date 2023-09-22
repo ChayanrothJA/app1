a0c580a3f2edc76bd8125f095b48d0d9de6ed423
Chayanroth_KMUTNB <110073989+ChayanrothJA@users.noreply.github.com>
2023-09-22 15:39:08 +0700

    app1 ROUTE

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react'
-import {userContext} from './context'
-import Header2 from './context-header2'
-import Content2 from './context-content2'
+import { BrowserRouter, Link } from 'react-router-dom'
+import Router1 from "./router1";
+import Router2 from "./router2";
+import Router3 from './router3';
+import Home from './home';
+import Product from './product';
+import Member from './member';
+import Contact from './contact';
 
+
+export default function App() {
+  
+   return (
+      <>
+         <Router3/>
+         
+
+      </>
+   )
+
+
+}
+
+
+
+// import {userContext} from './context'
+// import Header2 from './context-header2'
+// import Content2 from './context-content2'
 // import RefsFunc from './refs-func'
 // import RefsArray from './refs-array'
 // import React from 'react'
@@ -11,17 +35,30 @@ import Content2 from './context-content2'
 // import {EventData2} from './event-data'
 // import {Table} from './event-data'
 
+// export default function Router1(){
+//    return (
+//    <BrowserRouter>
+//    <nav>
+//    <Link to="/">Home</Link>
+//    <Link to="/products">Products</Link>
+   
+//    </nav>
+//    </BrowserRouter>
+//    )
+//    }
 
 
-export default function App() {
-   let[user,setUser] = React.useState('')
-   return (
-   <userContext.Provider value={[user,setUser]}>
-      <Header2/>
-      <Content2/>
-   </userContext.Provider>
-   ) 
-   }
+
+
+// export default function App() {
+//    let[user,setUser] = React.useState('')
+//    return (
+//    <userContext.Provider value={[user,setUser]}>
+//       <Header2/>
+//       <Content2/>
+//    </userContext.Provider>
+//    ) 
+//    }
 
 // function App() {
 //   return (
---
+++ src/member.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Member(){
+    return(
+        <p style={{textAlign:'center'}}>
+            <div>Member Page</div>
+        </p>
+    )
+}
