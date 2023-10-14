8dde17e75a4a1b7b33818602a49c0cfbf907d12f
ChayanrotJA <s6303051613084@email.kmutnb.ac.th>
2023-10-14 19:08:01 +0700

    Create form-post.js  &  index.js
    REST API Local & Server React.JS

---
+++ local/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FormPost from './form-post';
+import './index.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>แบบฟอร์มส่งข้อมูล</h1>
+      <FormPost/>
+    </div>
+  );
+}
